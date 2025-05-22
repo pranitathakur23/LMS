b450ef3cf099fbd2264c02bb4f2e2ec124c4a4f1
pritinocpl001 <priti@nocpl.in>
2025-05-22 12:51:58 +0530

    Field training option in sidebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -14,6 +14,7 @@ import { ReportsComponent } from './Reports/reports/reports.component';
 import { ResetpasswordComponent } from './Users/resetpassword/resetpassword.component';
 import { ProgressTrackerComponent } from './ProgressTracker/progress-tracker/progress-tracker.component';
 import { AddQuestionComponent } from './question-bank/add-question/add-question.component';
+import { FieldTrainingAssignmentComponent } from './AssignFieldTraining/field-training-assignment/field-training-assignment.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -35,6 +36,8 @@ export const routes: Routes = [
       { path: 'Mapping/course-mapping', component: CourseMappingComponent } , // Modified this path
       { path: 'Questions/questions', component: QuestionsComponent } , // Modified this path
       { path: 'ProgressTracker/progress-tracker', component: ProgressTrackerComponent } , 
+        { path: 'AssignFieldTraining/field-training-assignment', component: FieldTrainingAssignmentComponent } 
+
     ]// Modified this path    ]
   },
   { path: '', redirectTo: '/login', pathMatch: 'full' }
